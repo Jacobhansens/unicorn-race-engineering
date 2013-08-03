fb60debf47cfb01e556429fd205acd88da1b7bb5
Frederik Juul <Frederikj@gmail.com>
2013-08-03 18:39:16 +0200

    argh

--- g5/car_software/webinterface/bonescript/index.js
+++ g5/car_software/webinterface/bonescript/index.js
@@ -160,14 +160,14 @@ exports.Server = function(port, subdir, onconnect) {
     this.server = http.createServer();
     this.server.addListener('request', handler);
     if(socket.exists && (typeof onconnect == 'function')) {
-        var io6 = socket.listen(this.server6);
-        io6.sockets.on('connection', onconnect);
+        //var io6 = socket.listen(this.server6);
+        //io6.sockets.on('connection', onconnect);
         var io = socket.listen(this.server);
         io.sockets.on('connection', onconnect);
 		io.set('log level',1); // Stop logging to terminal
     }
     this.begin = function() {
-        this.server6.listen(port, '::0');
+        //this.server6.listen(port, '::0');
         this.server.listen(port);
     };
 };
